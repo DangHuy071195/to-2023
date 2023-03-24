1b0b9d542260f71cb1dbd90f8365d188e5ce3c7b
DangHuy071195 <nguyendanghuy071195@gmail.com>
2023-03-25 02:42:15 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './slide-show.style.scss'
 import HorizontalSlideShow from './component/HorizontalSlideShow'
+import images from './component/HorizontalSlideShow/mock-data-img'
 
 function App() {
 	return (
 		<div className='App'>
-			<HorizontalSlideShow listWidth={500} gutter={24} imgWidth={64} imgShow={5} />
+			<HorizontalSlideShow
+				data={images}
+				slideWidth={64}
+				carouselWidth={300}
+				maxVisibleSlide={5}
+				currentVisibleSlide={5}
+			/>
 		</div>
 	)
 }
 
 export default App
-
--- src/component/HorizontalSlideShow/index.tsx
+++ src/component/HorizontalSlideShow/index.tsx
@@ -2,42 +2,50 @@ import React, { useRef, useEffect, useState } from 'react'
 import images, { ImageDataI } from './mock-data-img'
 
 interface HorizontalSlideShowProps {
-	imgWidth: number
-	gutter: number
-	listWidth: number
-	imgShow: number
+	currentVisibleSlide: number
+	maxVisibleSlide: number
+	// fadeDistance: number
+	data: any
+	slideWidth: number
+	carouselWidth: number
 }
-const HorizontalSlideShow: React.FC<HorizontalSlideShowProps> = ({ imgWidth, gutter, listWidth, imgShow }) => {
+const HorizontalSlideShow: React.FC<HorizontalSlideShowProps> = (props) => {
 	const listContainerRef = useRef<HTMLUListElement | null>(null)
-	const [currentImageIdx, setCurrentImageIdx] = useState(Math.ceil(imgShow / 2) - 1)
-	const [totalWidth, setTotalWidth] = useState(0)
-	const [dataImgs, setDataImgs] = useState(images)
-	const [count, setCount] = useState(0)
-
+	const [data, setData] = useState(images)
+	const { currentVisibleSlide: currentVisibleDisplaySlide, maxVisibleSlide, slideWidth } = props
 	useEffect(() => {
-		const imgs = Array.from(listContainerRef.current!.children)
-		const imgWidth = imgs[0].getBoundingClientRect().width
-		setTotalWidth(imgShow * imgWidth)
-		console.log('imgWidth', imgWidth)
-		imgs.forEach((img: any, idx: number) => {
-			// img.style.left = imgWidth * idx + gutter + 'px'
-			img.style.left = imgWidth * idx + 'px'
-		})
+		// const imgs = Array.from(listContainerRef.current!.children)
+		// const imgWidth = imgs[0].getBoundingClientRect().width
+		// setTotalWidth(imgShow * imgWidth)
+		// console.log('imgWidth', imgWidth)
+		// imgs.forEach((img: any, idx: number) => {
+		// 	// img.style.left = imgWidth * idx + gutter + 'px'
+		// 	img.style.left = imgWidth * idx + 'px'
+		// })
+		if (data.length < (maxVisibleSlide + 1) / 2) {
+			throw Error('you must have more than (maxVisibleSlide + 1) / 2 data item')
+		}
+		if ((currentVisibleDisplaySlide && currentVisibleDisplaySlide % 2 !== 1) || maxVisibleSlide % 2 !== 1) {
+			throw Error('currentVisibleSlide or maxVisibleSlide must be an odd number')
+		}
+		if (currentVisibleDisplaySlide && currentVisibleDisplaySlide > maxVisibleSlide) {
+			throw Error('currentVisibleSlide must be smaller than maxVisibleSlide')
+		}
+		const currentVisibleSlides = currentVisibleDisplaySlide || maxVisibleSlide
 	}, [])
 
-	function moveElement(array: any[], fromIndex: number, toIndex: number) {
-		const arrayCopy = [...array]
-		const element = arrayCopy.splice(fromIndex, 1)[0]
+	// function moveElement(array: any[], fromIndex: number, toIndex: number) {
+	// 	const arrayCopy = [...array]
+	// 	const element = arrayCopy.splice(fromIndex, 1)[0]
 
-		console.log(element)
+	// 	console.log(element)
 
-		arrayCopy.splice(toIndex, 0, element)
+	// 	arrayCopy.splice(toIndex, 0, element)
 
-		return arrayCopy
-	}
+	// 	return arrayCopy
+	// }
 
 	const slideShowClickHandler = (e: any) => {
-		setCount((state) => ++state)
 		listContainerRef.current!.style.transform = `translateX(0)`
 		const targetImg = e.target.closest('li')
 		console.log('targetImg', targetImg)
@@ -47,57 +55,16 @@ const HorizontalSlideShow: React.FC<HorizontalSlideShowProps> = ({ imgWidth, gut
 		const currentImg = listContainerRef.current?.querySelector('.current--img')
 		const curIdx = Array.from(listContainerRef!.current!.children).findIndex((img) => img === currentImg)
 
-		moveToImg(listContainerRef, currentImg, targetImg, targetIdx, curIdx)
 		// setDataImgs(newArray!)
-	}
-
-	const moveToImg = (list: any, currentImg: any, targetImg: any, targetIdx: number, curIdx: number) => {
-		console.log('Math.ceil(imgShow / 2)')
-		setCurrentImageIdx(Math.ceil(imgShow / 2))
-		console.log('targetImg!.style.left', targetImg!.style.left)
-		console.log('targetIdx', targetIdx)
-		console.log('curIdx', curIdx)
-		console.log('sadas', targetImg!.style.left.replace('px', ''))
-		let leftCount = ''
-		if (count === 0) {
-			leftCount = `${(targetImg!.style.left.replace('px', '') - (targetIdx - curIdx) * imgWidth) / 2}px`
-		} else {
-			leftCount = `${targetImg!.style.left.replace('px', '') / 2 - (targetIdx - curIdx) * imgWidth}px`
-		}
-		list.current!.style.transform = `translateX(-${leftCount})`
-		// list.current!.style.transform = `translateX(-${(listContainerRef!.current!.getBoundingClientRect().width) / 2})`
-		// list.current!.style.transform = `translateX(-${listContainerRef!.current!.getBoundingClientRect().width / 2})`
-		// targetImg.style.transform = `scale(1.15)`
-		const imgs = Array.from(listContainerRef.current!.children)
-		dataImgs.forEach((img, idx) => {
-			if (imgShow < dataImgs.length && targetIdx > curIdx) {
-				// newArray = moveElement(dataImgs, idx, dataImgs.length - idx - 1)
-				//@ts-ignore
-				imgs[idx].style.right = imgWidth * (idx + 1) + 'px'
-				// dataImgs.splice(dataImgs.length - idx - 1, 0, dataImgs.splice(idx, 1)[0])
-			}
-		})
-
-		// setDataImgs(dataImgsUpdate)
-
-		// targetImg.style.transform = `translateX(50%)`
-		// targetImg.style.left = `50%`
-
-		currentImg?.classList.remove('current--img')
-		targetImg?.classList.add('current--img')
 	}
 
 	return (
 		<div className='slide-show'>
 			{/* @ts-ignore */}
-			<div className='slide-show__img--container' style={{ width: totalWidth }}>
-				{console.log('dataImgs', dataImgs)}
-
+			<div className='slide-show__img--container'>
 				<ul className='slide-show__img--container__list' ref={listContainerRef} onClick={slideShowClickHandler}>
 					{images.map((img: ImageDataI, idx: number) => (
-						<li
-							key={idx}
-							className={`slide-show__img--container__list--item ${idx === currentImageIdx ? 'current--img' : ''}`}>
+						<li key={idx} className={`slide-show__img--container__list--item `}>
 							<img src={img.url} alt='' />
 						</li>
 					))}
